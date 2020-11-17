a9803e33bf18e60529ece4affcfcd849959622e7
iskmar <iskrenovicmarko@gmail.com>
2020-11-17 15:38:12 +0100

    Added Welcome page to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import WelcomePage from "./Components/WelcomePage/WelcomePage";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <WelcomePage />
     </div>
   );
 }

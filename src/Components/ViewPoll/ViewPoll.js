c57c67becc65cedba66fb049bbec92d54621011d
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2020-11-22 15:58:03 +0100

    add votes to poll if active

--- src/Components/ViewPoll/ViewPoll.js
+++ src/Components/ViewPoll/ViewPoll.js
@@ -69,10 +69,14 @@ const ViewPoll = () => {
         newRestaurantVoteState[0].votes += 1;
           prevState[index] = newRestaurantVoteState[0]
         
-          PollsCollection.doc(pollId).update({
-            restaurants: prevState,
-            voters: firebase.firestore.FieldValue.arrayUnion('dusan')
-          })
+            if(data.active) {
+                PollsCollection.doc(pollId).update({
+                    restaurants: prevState,
+                    voters: firebase.firestore.FieldValue.arrayUnion('dusan')
+                  })
+            }
+
+         
         
         })
     }

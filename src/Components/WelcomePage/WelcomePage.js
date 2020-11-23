e8885dc35ee60473993fcc1a42ac0414b7db1850
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2020-11-23 23:29:33 +0100

    checking login with cookie

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,48 +1,50 @@
-import React from 'react';
-import './style.css';
-import Login from "../Login/Login"
-import SignUp from '../SignUp/SignUp';
-import { Route, Switch, useHistory, useLocation } from 'react-router-dom'
+import React from "react";
+import Login from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
+import { Route, Switch, useHistory, useLocation } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+import "./style.css";
 
 const WelcomePage = () => {
+  const [cookies, setCookie] = useCookies(["user"]);
 
-    const location = useLocation()
-    const history = useHistory()
+  const location = useLocation();
+  const history = useHistory();
 
-    const isLogin = () => {
-
-        if (localStorage.getItem("status") === null) {
-            return false
-        }
-        else {
-            history.push('/home')
-        }
+  const isLogin = () => {
+    if (cookies.user === undefined) {
+      return false;
+    } else {
+      history.push("/home");
     }
-    const showText = () => {
-        if (location.pathname === '/') {
-            return (
-                <h3 className="h3-welcome-page">Easy solution for company food ordering!</h3>
+  };
 
-            )
-        }
-        else return
-    }
+  const showText = () => {
+    if (location.pathname === "/") {
+      return (
+        <h3 className="h3-welcome-page">
+          Easy solution for company food ordering!
+        </h3>
+      );
+    } else return;
+  };
 
+  return (
+    <div className="welcome-wrapper">
+      <h1 className="h1-welcome-page">
+        ARE YOU <br></br> HUNGRY?
+      </h1>
 
-    return (
-
-        <div className="welcome-wrapper">
-            <h1 className="h1-welcome-page">ARE YOU <br></br> HUNGRY?</h1>
-
-            {isLogin()}
-            {showText()}
-            {isLogin()}
-            <Switch>
-                <Route component={Login} exact path='/login' />
-                <Route component={SignUp} exact path='/signup' />
-            </Switch>
-        </div>
-    );
-}
+      {isLogin()}
+      {showText()}
+      {isLogin()}
+      <Switch>
+        <Route component={Login} exact path="/login" />
+        <Route component={SignUp} exact path="/signup" />
+      </Switch>
+    </div>
+  );
+};
 
 export default WelcomePage;

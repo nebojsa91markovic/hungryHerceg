7e75ce80c368915735fce48987e9cf64b034c290
Banovacki Ivan <banovacki.ivan@gmail.com>
2020-11-27 11:39:02 +0100

    Order Item

--- src/Components/OrderItem/OrderItem.js
+++ src/Components/OrderItem/OrderItem.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import "./style.css";
+import { v4 as uuidv4 } from "uuid";
 
 const OrderItem = ({ order, id }) => {
   const finishAt = moment(order.created).add(30, "minutes");
@@ -27,7 +28,7 @@ const OrderItem = ({ order, id }) => {
   };
 
   return (
-    <Link disabled className="orderItem" to={!isItFinished() ? `/order/${order.id}` : '/orders'}>
+    <Link key={uuidv4()} className="orderItem" to={!isItFinished() ? `/order/${order.id}` : '/orders'}>
       <h3>{order.label}</h3>
       <span>{timeLeft}</span>
       <span>{isItFinished() ? "Order is created" : "Not yet!"}</span>
